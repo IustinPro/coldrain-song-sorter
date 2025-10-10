1fbe1932aa513cf633786c905f1926671bff66e0
IustinPro <iustin.jocuri1@gmail.com>
2025-10-10 22:57:42 +0100

    Update fnc_data_v2.js
    changed songs from BM to Coldrain

--- js/bm/fnc_data_v2.js
+++ js/bm/fnc_data_v2.js
@@ -1,5 +1,6 @@
 ﻿// 2008/7/3 Scripted by K-Factory@migiwa
 // 2009/1/27 Modified by K-Factory@migiwa
+// 2025/10/10 coldrain Fork by IustinPro
 
 // *****************************************************************************
 str_CenterT = 'Tie!';
@@ -25,13 +26,23 @@ var bln_ProgessBar = 1;
 var maxRows = 20;
 
 // * タイトル情報（編集可能。最後の行に”,”を付けないようにしてください）
-var int_Colspan = 3;
+var int_Colspan = 14;
 var ary_TitleData = [
-  "BABYMETAL",
-  "METAL RESISTANCE",
+  "Until The End",
+  "Optimize",
+  "Through Clarity",
+  "Nothing Lasts Forever",
+  "Final Destination (Re-Recorded)",
+  "Nonnegative",
+  "The Side Effects",
+  "Fateless",
+  "Vena",
+  "The Revelation",
+  "The Enemy Inside",
+  "Final Destination",
+  "Maxi Singles",
   "Other"
 ];
-
 // * キャラクター情報（編集可能。最後の行に”,”を付けないようにしてください）
 // * 使用フラグ（0にするとソートに入りません）, 
 //   "タイトルID"（先頭から0, 1, 2...）, 
@@ -39,46 +50,154 @@ var ary_TitleData = [
 //   "キャラクター名", "画像（空白の場合、キャラクター名が使用されます）"
 //                                      [1,2,3,4,5,6,7,8,9,
 var ary_CharacterData = [
-  [1, "Babymetal Death",            [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Megitsune",                  [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Gimme Chocolate!!",          [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Ii ne!",                      [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Akatsuki",                   [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Akatsuki (Unfinished ver.)", [1,0,0], "bmsong/bm02.jpg"],
-  [1, "Doki Doki Morning",          [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Onedari Daisakusen",         [1,0,0], "bmsong/bm01.jpg"],
-  [1, "4 no Uta",                   [1,0,0], "bmsong/bm01.jpg"],
-  [1, "4 no Uta (444 ver.)",        [1,0,0], "bmsong/bm02.jpg"],
-  [1, "Uki Uki Midnight",           [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Catch Me If You Can",        [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Rondo of Nightmare",         [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Headbangeeeeerrrrr!!!!!",    [1,0,0], "bmsong/bm01.jpg"],
-  [1, "Ijime, Dame, Zettai",        [1,0,0], "bmsong/bm01.jpg"],
+// Until The End
+[1, "House Of Cards", [1,0,0,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_ute"],
+[1, "Until The End", [1,0,0,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_ute"],
+[1, "New Dawn", [1,0,0,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_nd"], // Added Here to Spare another var
 
-  [1, "Road of Resistance",         [0,1,0], "bmsong/mr01.jpg"],
-  [1, "Karate",                     [0,1,0], "bmsong/mr01.jpg"],
-  [1, "Awadama Fever",              [0,1,0], "bmsong/mr01.jpg"],
-  [1, "Yava!",                      [0,1,0], "bmsong/mr01.jpg"],
-  [1, "Amore",                      [0,1,0], "bmsong/mr01.jpg"],
-  [1, "Meta Taro",                  [0,1,0], "bmsong/mr01.jpg"],
-  [1, "Syncopation",                [0,1,0], "bmsong/mr02.jpg"],
-  [1, "From Dusk Till Dawn",        [0,1,0], "bmsong/mr01.jpg"],
-  [1, "GJ!",                        [0,1,0], "bmsong/mr01.jpg"],
-  [1, "GJ! (Reward edit)",          [0,1,0], "bmsong/mr03.jpg"],
-  [1, "Sis. Anger",                 [0,1,0], "bmsong/mr01.jpg"],
-  [1, "No Rain, No Rainbow",        [0,1,0], "bmsong/mr01.jpg"],
-  [1, "Tales of the Destinies",     [0,1,0], "bmsong/mr01.jpg"],
-  [1, "The One",                    [0,1,0], "bmsong/mr02.jpg"],
-  [1, "The One (English ver.)",     [0,1,0], "bmsong/mr01.jpg"],
-  [1, "The One (Unfinished ver.)",  [0,1,0], "bmsong/mr03.jpg"],
 
-  [1, "Kimi to Anime ga Mitai",     [0,0,1], "bm.png"],
-  [1, "Mischiefs of metal gods",    [0,0,1], "bm.png"],
-  [1, "Ii ne!(Vega mix ver.)",      [0,0,1], "bm.png"],
-  [1, "Love machine",               [0,0,1], "bm.png"],
-  [1, "Chokotto Love",              [0,0,1], "bm.png"],
-  [1, "White Love",                 [0,0,1], "bm.png"],
-  [1, "Over The Future",            [0,0,1], "bm.png"],
-  [1, "Tsubasa wo Kudasai",         [0,0,1], "bm.png"],
-  [1, "Soul's Refrain",             [0,0,1], "bm.png"],
+// Optimize
+[1, "FREE FALL", [0,1,0,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_o"],
+[1, "INCOMPLETE", [0,1,0,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_o"],
+[1, "CHASING SHADOWS", [0,1,0,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_o"],
+
+// Through Clarity
+[1, "No Escape", [0,0,1,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_tc"],
+[1, "Persona", [0,0,1,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_tc"],
+[1, "The Future", [0,0,1,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_tc"],
+[1, "Six Feet Under", [0,0,1,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_tc"],
+[1, "Never Look Away", [0,0,1,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_tc"],
+[1, "Inside of Me", [0,0,1,0,0,0,0,0,0,0,0,0,0,0], "crsong/cr_tc"],
+
+// Nothing Lasts Forever
+[1, "Die Tommorow", [0,0,0,1,0,0,0,0,0,0,0,0,0,0], "crsong/cr_nlf"],
+[1, "Were Not Alone", [0,0,0,1,0,0,0,0,0,0,0,0,0,0], "crsong/cr_nlf"],
+[1, "Stuck", [0,0,0,1,0,0,0,0,0,0,0,0,0,0], "crsong/cr_nlf"],
+[1, "After Dark", [0,0,0,1,0,0,0,0,0,0,0,0,0,0], "crsong/cr_nlf"],
+[1, "The Youth", [0,0,0,1,0,0,0,0,0,0,0,0,0,0], "crsong/cr_nlf"],
+[1, "Miss You", [0,0,0,1,0,0,0,0,0,0,0,0,0,0], "crsong/cr_nlf"],
+
+// Final Destination (Re-Recorded)
+[1, "Final Destination (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "Counterfits & Lies (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "Someday (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "Fiction (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "Just Tonight (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "24-7 (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "Doors (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "Deja Vu (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "Survive (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "My Addiction (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "Painting (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "8AM (Re-Recorded)", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+[1, "Vengeance", [0,0,0,0,1,0,0,0,0,0,0,0,0,0], "crsong/cr_fdr"],
+
+// Nonnegative
+[1, "Help Me Help You", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "Calling", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "Cut Me", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "Before I Go", [0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "Bloody Power Fame", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "Here With You", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "Boys And Girls", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "Paradise (Kill the SIlence)", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "2020", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "Rabbit Hole", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "Dont Speak", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+[1, "From Today", [0,0,0,0,0,1,0,0,0,0,0,0,0,0], "crsong/cr_nn"],
+
+// The Side Effects
+[1, "Mayday", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "Coexist", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "See You", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "Speak", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "The Side Effects", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "January 1st", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "Insomnia", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "Answer/Sickness", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "Breathe", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "Stay The Course", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "Revolution", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+[1, "Li(e)fe", [0,0,0,0,0,0,1,0,0,0,0,0,0,0], "crsong/cr_tsf"],
+
+// Fateless
+[1, "Envy", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "Feed the Fire", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "Lost in Faith", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "Bury Me", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "R.I.P", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "Inside Out", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "Stay", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "Colorblind", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "F.T.T.T", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "Uninvited", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "Aftermath", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+[1, "A Decade in the Rain", [0,0,0,0,0,0,0,1,0,0,0,0,0,0], "crsong/cr_f"],
+
+// Vena (Vena II included)
+[1, "Vena", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Wrong", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Divine", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Gone", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Words of the Youth", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "The Story", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Whole", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Runaway", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Pretty Little Liar", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Heart of the Young", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Fire in the Sky", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Born to Bleed", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Undertow", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "Gone (Acoustic)", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+[1, "The Story (Acoustic)", [0,0,0,0,0,0,0,0,1,0,0,0,0,0], "crsong/cr_v"],
+
+// The Revelation
+[1, "The War is On", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "The Revelation", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "Fade Away", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "Given Up on You", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "Time Bomb", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "You Lie", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "Evolve", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "Behind the Curtain", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "Aware and Awake", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "Voiceless", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+[1, "Chasing Dreams", [0,0,0,0,0,0,0,0,0,1,0,0,0,0], "crsong/cr_tr"],
+
+// The Enemy Inside
+[1, "To Be Alive", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+[1, "New Fate", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+[1, "Rescue Me", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+[1, "Adrenaline", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+[1, "You", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+[1, "The Maze", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+[1, "Rise and Fall", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+[1, "Confession", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+[1, "A Tragic Instinct", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+[1, "Hollow", [0,0,0,0,0,0,0,0,0,0,1,0,0,0], "crsong/cr_tei"],
+
+// FInal Destination
+[1, "Final Destination", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "Counterfits & Lies", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "Someday", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "Fiction", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "Just Tonight", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "24-7", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "Doors", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "Deja Vu", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "Survive", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "My Addiction", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "Painting", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+[1, "8AM", [0,0,0,0,0,0,0,0,0,0,0,1,0,0], "crsong/cr_fd"],
+
+// Maxi Singles
+[1, "Time to Go", [0,0,0,0,0,0,0,0,0,0,0,0,1,0], "crsong/cr_8"],
+[1, "Believe", [0,0,0,0,0,0,0,0,0,0,0,0,1,0], "crsong/cr_8"],
+[1, "Fiction (Unpluged)", [0,0,0,0,0,0,0,0,0,0,0,0,1,0], "crsong/cr_8"],
+[1, "Come Awake", [0,0,0,0,0,0,0,0,0,0,0,0,1,0], "crsong/cr_fi"],
+[1, "I Know", [0,0,0,0,0,0,0,0,0,0,0,0,1,0], "crsong/cr_8"],
+
+// Other
+[1, "Been so Long", [0,0,0,0,0,0,0,0,0,0,0,0,0,1], "crsong/cr_1"],
+
 ];
